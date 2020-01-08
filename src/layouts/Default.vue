a465b6bf4211a335632c854ebbfcfe63db2ab0d4
J. Alexander Curtis <jalexandercurtis@gmail.com>
2020-01-07 23:56:40 -0700

    Added basic website layout and pages other than Journal

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,7 +2,7 @@
   <div class="layout">
     <header class="header">
       <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+        <g-link class="brand" to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
@@ -23,7 +23,7 @@ query {
 }
 </static-query>
 
-<style>
+<style lang="scss">
 body {
   font-family: 'Nunito Sans',-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
   margin:0;
@@ -46,7 +46,26 @@ body {
   height: 80px;
 }
 
+.brand {
+  color: #ff3f34;
+  text-decoration: none;
+  font-family: 'Nunito Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 1.4em;
+}
+
 .nav__link {
+  color: #2D3748;
   margin-left: 20px;
+  text-decoration: none;
+  font-family: 'Nunito Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 1em;
+
+  &.active--exact {
+    color: #ff3f34;
+  }
+
+  &:hover {
+    color: #718096;
+  }
 }
 </style>
---
+++ src/pages/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>Contact Me</h1>
+    <p>I'd love to hear from you. Feel free to reach out to me through email, twitter, or any other means.</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About us'
+  }
+}
+</script>
