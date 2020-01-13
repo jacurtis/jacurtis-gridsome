8f9eae0c28082b66a5f6dfbd4d8f22134ee240ed
J. Alexander Curtis <jalexandercurtis@gmail.com>
2020-01-12 21:59:23 -0700

    Added youtube, github, and twitter icons to Icon component

--- src/components/Site/Footer.vue
+++ src/components/Site/Footer.vue
@@ -2,12 +2,15 @@
     <footer>
         <div class="container">
             <div class="footer-content">
-                <div class="footer-message">Helping you build and grow your next great idea</div>
+                <div class="footer-message">
+                    <!-- <h4>Jacurtis</h4> -->
+                    <p>Helping you build and grow your next great idea</p>
+                </div>
                 <div class="footer-links">
                     <ul>
-                        <li><a href="https://youtube.com">YouTube</a></li>
-                        <li><a href="https://twitter.com/_jacurtis">Twitter</a></li>
-                        <li><a href="https://github.com/jacurtis">Github</a></li>
+                        <li><a href="https://youtube.com"><jac-icon class="youtube-icon" name="youtube" size="25"></jac-icon></a></li>
+                        <li><a href="https://twitter.com/_jacurtis"><jac-icon class="twitter-icon" name="twitter" size="25"></jac-icon></a></li>
+                        <li><a href="https://github.com/jacurtis"><jac-icon class="github-icon" name="github" size="25"></jac-icon></a></li>
                     </ul>
                 </div>
             </div>
@@ -15,15 +18,31 @@
     </footer>
 </template>
 
+<script>
+import JacIcon from '~/components/Icon';
+export default {
+    components: {
+        JacIcon,
+    }
+}
+</script>
+
 <style lang="scss" scoped>
 footer {
     background-color: #f2f2f2;
     padding: 60px 10%;
+    margin-top: 100px;
+    font-size: 1.2rem;
 
     .footer-content {
         display: flex;
         align-items: center;
         justify-content: space-between;
+
+        h4 {
+            font-family: basic-sans, sans-serif;
+            font-weight: 700;
+        }
     }
 }
 .footer-links {
@@ -35,13 +54,31 @@ footer {
     }
 
     li {
-    float: left;
+        float: left;
     }
 
     li a {
-    display: block;
+    display: flex;
+    align-items: center;
     padding: 16px;
     text-decoration: none;
+    color: #c6cbd1;
+
+        .github-icon:hover {
+            color: #24292e;
+        }
+
+        .twitter-icon:hover {
+            color: rgb(29, 161, 242);
+        }
+
+        .youtube-icon:hover {
+            color: #FF0000;
+        }
+    
+        svg {
+            fill: currentColor;
+        }
     }
 }
 </style>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,6 +2,14 @@
   <Layout>
     <h1>Contact Me</h1>
     <p>I'd love to hear from you. Feel free to reach out to me through email, twitter, or any other means.</p>
+    <h3>Contracting & Freelance</h3>
+    <p>
+      Looking for a developer? I'd love to chat through your requirements & help you build something awesome. Please reach out to me via email and I'll get back to you as soon as I can.
+    </p>
+    <h3>Anything Else</h3>
+    <p>
+      If you want to get in touch to chat about programming, product or anything else, flick me an email or send me a DM on twitter. I'd love to hear from you!
+    </p> 
   </Layout>
 </template>
 
