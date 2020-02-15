71c4177a4a6ab072b6fe5576ea270a5d48b08b1f
J. Alexander Curtis <jalexandercurtis@gmail.com>
2020-02-15 10:00:28 -0800

    📱 Added Mobile Menu.

--- src/components/Site/Header.vue
+++ src/components/Site/Header.vue
@@ -3,16 +3,93 @@
     <strong>
       <g-link class="brand" to="/">JACURTIS</g-link>
     </strong>
-    <nav class="nav">
-      <g-link class="nav__link" to="/" exact>Home</g-link>
-      <g-link class="nav__link" to="/about/">About</g-link>
-      <g-link class="nav__link" to="/journal">Journal</g-link>
-      <g-link class="nav__link" to="/contact/">Contact</g-link>
+    <hamburger-icon :active="menuActive" @toggle="toggleMenu"></hamburger-icon>
+    <div class="background-overlay" @click="toggleMenu(false)" :class="{'active':menuActive}"></div>
+    <nav class="nav" :class="{'active':menuActive}">   
+      <div class="link-list">
+        <g-link class="nav__link" to="/" exact>Home</g-link>
+        <g-link class="nav__link" to="/about/">About</g-link>
+        <g-link class="nav__link" to="/journal">Journal</g-link>
+        <g-link class="nav__link" to="/contact/">Contact</g-link>
+      </div>
     </nav>
   </header>
 </template>
 
+<script>
+import HamburgerIcon from '~/components/Site/HamburgerIcon'
+export default {
+  data: function() {
+    return {
+      menuActive: false
+    }
+  },
+  components: {
+    HamburgerIcon
+  },
+  methods: {
+    toggleMenu: function(val) {
+      this.menuActive = val
+    }
+  }
+}
+</script>
+
 <style lang="scss" scoped>
+  @import "~/assets/scss/_variables";
+  @import "~/assets/scss/_media-queries";
+
+  @include touch {
+    .background-overlay {
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      background-color: rgba(45, 55, 72, 0.0);
+      z-index: 99;
+      transform: translateX(100%);
+      transition: background-color 125ms ease-in-out 125ms;
+
+      &.active {
+        background-color: rgba(45, 55, 72, 0.25);
+        transform: translateX(0);
+      }
+    }
+    .nav {
+      width: 90%;
+      max-width: 400px;
+      background-color: white;
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      border-radius: 10px 0 0 10px;
+      transform: translateX(100%);
+      transition: transform 250ms ease-in-out, box-shadow 250ms ease-in-out;
+      z-index: 100;
+      &.active {
+        transform: translateX(0%);
+        box-shadow: 0px 0 25px 10px rgba(45, 55, 72, 0.3);
+
+        &::after {
+          transform: translateX(0);
+        }
+      }
+
+      .link-list {
+        display: flex;
+        flex-direction: column;
+        text-align: right;
+        padding: 100px 25px 0 25px;
+
+        .nav__link {
+          padding: 8px 0;
+        }
+      }
+    }
+  }
+
   .brand {
     font-size: 1.3rem;
   }
