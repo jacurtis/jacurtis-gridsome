93807aea788d63dbdbd20e932761913c60932d09
J. Alexander Curtis <jalexandercurtis@gmail.com>
2020-02-15 07:15:46 -0800

    💔 Seperated css files into site-wide stylesheet. Resolves #12

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -29,70 +29,6 @@ export default {
 </script>
 
 <style lang="scss">
-h1, h2, h3, h4, .h1, .h2, .h3, .h4 {
-  font-family: montserrat, sans-serif;
-  font-weight: 700;
-}
-
-h1, .h1 {
-  font-size: 2.3rem;
-  font-weight: 900;
-}
-
-h2, .h2 {
-  font-size: 2rem;
-}
-
-h3 {
-  font-size: 1.8rem;
-  font-weight: 400;
-  margin-bottom: 5px;
-}
-
-h4 {
-  font-size: 1.5rem;
-  font-weight: 400;
-}
-
-body {
-  font-family: merriweather, serif;
-  font-weight: 400;
-  font-size: 10px;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-p {
-  font-size: 16px;
-  margin-top: 0;
-}
-
-.container {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.brand {
-  color: #ff3f34;
-  text-decoration: none;
-  font-family: 'Nunito Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  font-size: 1.4em;
-}
-
-.hover-brand:hover {
-  color: #ff3f34;
-}
 
 .top-accent-bar {
   width: 100%;
@@ -104,9 +40,5 @@ p {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-}
-
-.flex-1 {
-  flex: 1;
 }
 </style>
